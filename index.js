74a7bfce5ee0245a2df9639e56de408a8ae458f1
George <georgejoj28@gmail.com>
2023-09-16 07:54:09 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const messages = []
+
+function writeCards(names, event) {
+  for (let i = 0;names.length > i; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+  }
+  return messages
+}
+
+function countDown() {
+    let count = 0;
+    while (count < 11) {
+        console.log(count++)
+    }
+}
+
